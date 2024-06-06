eed49d65d8845cdb21a60b9ec26d47fa314a4fa0
Immanuel <immanuel@Immanuels-MacBook-Air.local>
2024-06-06 12:03:31 +0000

    trial sheet removed

--- trialsheet.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
-
-//register emitter 
-emitter.on("messageLog", function(args){
-    console.log("shape dragged on positions: ", args);
-})
-
-emitter.emit("messageLog", { "x":23 , "y":-23});
